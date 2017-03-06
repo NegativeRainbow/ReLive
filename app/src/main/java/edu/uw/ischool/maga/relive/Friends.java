279b61584f654a1926c8480faa019c63790c302d
Kito Pham <kpham97@live.com>
2017-03-06 00:59:42 -0800

    fixed friend object

--- app/src/main/java/edu/uw/ischool/maga/relive/Friends.java
+++ app/src/main/java/edu/uw/ischool/maga/relive/Friends.java
@@ -5,4 +5,11 @@ package edu.uw.ischool.maga.relive;
  */
 
 public class Friends {
+    public String name;
+    public int id;
+
+    public Friends(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
 }
